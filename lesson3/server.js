5b4b305bc9e4612824ffcd40c76040c343081c89
StroeJohnCiprian <78986313+StroeJohnCiprian@users.noreply.github.com>
2023-02-02 22:41:58 +0200

    init

--- lesson3/server.js
+++ lesson3/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write('<p> good morning USA </p>');
+    res.write('<p> good morning USA, again </p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {

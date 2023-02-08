107178980f86d9a2cf52f7df5acf0588df1e0b40
StroeJohnCiprian <78986313+JohnStroe@users.noreply.github.com>
2023-02-08 18:33:19 +0200

    init

---
+++ lesson3/archive/oldserver.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="styleseet" href="#"> </head>')
+    res.write('<p> good morning USA </p>');
+    res.write('<p> good morning USA, again </p>');
+    res.end();
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000')
+});

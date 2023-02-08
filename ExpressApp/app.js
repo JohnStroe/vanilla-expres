30c280b76bea828cd6ab28efefd1636d6a5f5b63
StroeJohnCiprian <78986313+JohnStroe@users.noreply.github.com>
2023-02-08 19:34:27 +0200

    init

---
+++ ExpressApp/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// set up express ap
+const app = express();
+
+//listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) =>{
+
+    // res.send('<p> Home Page </p>')
+    res.sendFile('./views/index.html', { root: __dirname});
+
+});
+
+app.get('/about', (req, res) =>{
+
+    // res.send('<p> About Page </p>')
+    res.sendFile('./views/about.html', { root: __dirname});
+
+});
